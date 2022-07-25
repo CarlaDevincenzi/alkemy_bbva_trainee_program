63c85c6f79219297c62c90fe7d5cef871f6460d4
CarlaDevincenzi <anabellacarla2301@gmail.com>
2022-07-25 02:35:35 -0300

    Validacion de formulario

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,36 @@
 function Login(){
+
+    const submitHandler = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if(email === '' || password === ''){
+            console.log("Los campos no pueden estar vacios");
+            return;
+        }
+
+        if(email !== '' && !regexEmail.test(email)){
+            console.log("Debes escribir una direccion de correo valida");
+            return;
+        }
+
+        if(email !== 'carladev@alkemy.org' || password !== 'react'){
+            console.log("Credenciales invalidas");
+            return;
+        }
+
+        console.log("Ok, estamos listos para enviar la informacion");
+    }
     return(
         <>
             <h2>Formulario de Login</h2>
-            <form>
+            <form onSubmit = {submitHandler}>
                 <label>
                     <span>Correo electronico</span> <br />
-                    <input type="email" name="email" />
+                    <input type="text" name="email" />
                 </label>
                 <br />
                 <label>
