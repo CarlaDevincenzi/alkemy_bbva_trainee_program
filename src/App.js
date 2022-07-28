0d83f13c0c1bc11a564c46b6c7aa4dfe402b77ad
CarlaDevincenzi <anabellacarla2301@gmail.com>
2022-07-27 23:03:22 -0300

    integracion del Header y Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import {Routes, Route} from "react-router-dom";
 import Login from './components/Login';
 import Listado from './components/Listado';
-import {Routes, Route} from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (       
     <>
+      <Header />
+      
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/listado" element={<Listado/>} />       
       </Routes>
+
+      <Footer />
     </>    
   );
 }
