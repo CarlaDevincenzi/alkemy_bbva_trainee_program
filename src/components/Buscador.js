292273a18e38a82ce0546199e95b49febbbc6249
CarlaDevincenzi <anabellacarla2301@gmail.com>
2022-08-08 14:10:54 -0300

    Buscador: validacion del formulario de busqueda

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -3,11 +3,13 @@ import swAlert from '@sweetalert/with-react';
 function Buscador() {
 const submitHandler = (e) => {
     e.preventDefault();
-    const keyWord = e.currentTarget.keyWord.value;
+    const keyWord = e.currentTarget.keyWord.value.trim();
     console.log(keyWord);
 
     if(keyWord.length === 0){
-        swAlert(<h2>Tiene que ingresar al menos una palabra</h2>);
+        swAlert(<h5>Tiene que ingresar al menos una palabra</h5>);
+    }else if(keyWord.length < 4){
+        swAlert(<h5>Tiene que ingresar al menos 4 caracteres</h5>);
     }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,13 @@ function Header() {
             <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to={"#"}>Alkemy</Link>
+                    <Buscador /> 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                      
                     <div className="collapse navbar-collapse" id="navbarNav">
+                    
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to={"#"}>Home</Link>
@@ -21,7 +23,7 @@ function Header() {
                             </li>        
                         </ul>                                               
                     </div> 
-                    <Buscador />                  
+                                     
                 </div>                
             </nav>                       
         </header>
